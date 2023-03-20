43ac31d64a5f901a4c1c9ddee86714e94e1885d9
Rahmanr060902 <98864642+Rahmanr060902@users.noreply.github.com>
2023-03-20 18:01:58 +0700

    latihan2

---
+++ src/raluy.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class raluy {
+    public static void main(String[] args) {
+        // Membuat objek Hewan
+        ArrayList<String> hewan = new ArrayList<>();
+        hewan.add("Kucing");
+        hewan.add("Anjing");
+        hewan.add("Burung");
+        hewan.add("Ikan");
+
+        // Membuat objek DeleteHewan
+        ArrayList<String> deleteHewan = new ArrayList<>();
+        deleteHewan.add("Anjing");
+        deleteHewan.add("Ikan");
+    }
+}
+
